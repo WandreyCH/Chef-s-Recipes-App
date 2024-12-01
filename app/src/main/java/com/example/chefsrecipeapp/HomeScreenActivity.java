9f99e52230074b4ce6f69c11820b9a5ddcd57835
Wandrey C. Haagensen <142623622+WandreyCH@users.noreply.github.com>
2024-12-01 18:24:27 -0500

    add searchbar to recipe detail screen

--- app/src/main/java/com/example/chefsrecipeapp/HomeScreenActivity.java
+++ app/src/main/java/com/example/chefsrecipeapp/HomeScreenActivity.java
@@ -7,15 +7,20 @@ import android.widget.TextView;
 import android.widget.RatingBar;
 
 public class HomeScreenActivity extends AppCompatActivity {
+
+    EditText searchBar;
+    TextView topRatedTitle;
+    RatingBar ratingBar1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
         // Inicialize componentes (pode ser útil para lógica futura)
-        EditText searchBar = findViewById(R.id.searchBar);
-        TextView topRatedTitle = findViewById(R.id.topRatedTitle);
-        RatingBar ratingBar1 = findViewById(R.id.ratingBar1);
+         searchBar = findViewById(R.id.searchBar);
+         topRatedTitle = findViewById(R.id.topRatedTitle);
+         ratingBar1 = findViewById(R.id.ratingBar1);
 
         // Configurações ou dados dinâmicos podem ser carregados aqui
     }
